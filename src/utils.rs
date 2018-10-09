22116c28a9d70b6e9a26c4cc526eb47a322a8f08
Nick Fitzgerald <fitzgen@gmail.com>
2018-10-09 10:54:58 -0700

    Global allocators have to be defined in the root of the crate
    At least until https://github.com/rust-lang/rust/issues/44113 is fixed.

--- src/utils.rs
+++ src/utils.rs
@@ -11,13 +11,3 @@ cfg_if! {
         pub fn set_panic_hook() {}
     }
 }
-
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
